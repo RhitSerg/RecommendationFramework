81d031f76bda271dfd64992b3378f71ba9454ea0
Kurtis Zimmerman <zimmerka@rose-hulman.edu>
2013-02-10 15:21:02 -0500

    Setting up test workspace

---
+++ TestWorkspace/Tests/src/sum/E2.java
@@ -0,0 +1,32 @@
+package sum;
+
+public class E2 {
+	public void source() {
+		int sum = 0;
+		for (int i = 0; i >= 10; i++) {
+			sum = sum - i;
+		}
+	}
+	
+	public void target() {
+		int sum = 0;
+		for (int i = 0; i < 10; i++) {
+			sum = sum + 1;
+		}
+	}
+}
+/*
+ * Actual differences:
+ * (R) 6: '>=' operator
+ * (R) 7: '-' operator
+ * (R) 7: 'i' identifier
+ * 
+ * Expected difference score:
+ * ???
+ * 
+ * Expected recommended edits:
+ * 6: '>=' -> '<'
+ * 7: '-' -> '+'
+ * 7: 'i' -> '1'
+ * 
+ */
--- src/astrecognition/Settings.java
+++ src/astrecognition/Settings.java
@@ -4,7 +4,7 @@ import astrecognition.visitors.GeneralVisitor;
 import astrecognition.visitors.SimplifierVisitor;
 
 public class Settings {
-	public static int P = 4;
+	public static int P = 2;
 	public static int Q = 3;
 	public static Class<? extends GeneralVisitor> VISITOR_CLASS = SimplifierVisitor.class;
 }
