d87e2edbcc92cc82a3d6ede092ad62e90befba8b
Richard Thai <thairp@rose-hulman.edu>
2013-02-26 22:25:39 -0500

    a search and sort test were added

---
+++ TestWorkspace/Tests/src/sequentialSearch/E0.java
@@ -0,0 +1,27 @@
+public class E0 {
+	public void source(int[] a, int b) {
+	    for (int i : a) {
+	        if (i==b){
+	          return true;
+	        }
+	      }
+	      return false; 
+	    }
+
+	public void source(int[] a, int b) {
+	    for (int i : a) {
+	        if (i==b){
+	          return true;
+	        }
+	      }
+	      return false; 
+	    }
+	}
+}
+/*
+ * Actual differences: None
+ * 
+ * Expected difference score: 0.0
+ * 
+ * Expected recommended edits: 6: None
+ */
--- TestWorkspace/Tests/src/zdummy/Main.java
+++ TestWorkspace/Tests/src/zdummy/Main.java
@@ -1,5 +1,10 @@
 package zdummy;
 
+/*
+ * TODO: CFG needs a Main class to operate without crashing.
+ * Make it so it doesn't
+ */
+
 public class Main {
 	public static void main(String args[]) {
 		return;
