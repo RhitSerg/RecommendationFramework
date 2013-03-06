710cf37b7d1c4927bd4a65b3f92ac6ae1234d1c0
Kurtis Zimmerman <zimmerka@rose-hulman.edu>
2013-03-06 13:26:06 -0500

    test runs through all classes in project

--- src/astrecognition/actions/PQGramTestAction.java
+++ src/astrecognition/actions/PQGramTestAction.java
@@ -1,5 +1,6 @@
 package astrecognition.actions;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,12 +11,14 @@ import pqgram.PQGram;
 import pqgram.PQGramRecommendation;
 import pqgram.edits.Edit;
 import soot.toolkits.graph.ExceptionalUnitGraph;
+import astrecognition.ASTBuilder;
 import astrecognition.Settings;
 import astrecognition.model.Tree;
 import astrecognition.views.AbstractView;
 import astrecognition.views.ClassPickerDialog;
 import astrecognition.views.ProjectPickerDialog;
 import cfgrecognition.actions.CFGExceptionalUnitGraphAction;
+import cfgrecognition.loader.SootClassLoader;
 import cfgrecognition.model.CFG;
 
 public class PQGramTestAction extends PQGramAction {
@@ -25,7 +28,7 @@ public class PQGramTestAction extends PQGramAction {
 		this.parentView = parentView;
 	}
 
-	@Override
+	/*@Override
 	public void run() {
 		ProjectPickerDialog projectPicker = new ProjectPickerDialog(Display
 				.getDefault().getActiveShell());
@@ -67,6 +70,56 @@ public class PQGramTestAction extends PQGramAction {
 				message = "ASTs not formatted as expected";
 			}
 			this.parentView.showMessage(title, message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
+	
+	@Override
+	public void run() {
+		ProjectPickerDialog projectPicker = new ProjectPickerDialog(Display
+				.getDefault().getActiveShell());
+		String title, message;
+		CFGExceptionalUnitGraphAction.instantiateProject();
+		SootClassLoader loader = SootClassLoader.instance();
+		try {
+			IJavaProject javaProject = projectPicker.getProject();
+			Collection<Tree> classes = ASTBuilder.getPackages(javaProject.getProject(),	Settings.VISITOR_CLASS);
+			for (Tree classTree : classes) {
+				try {
+					String className = classTree.getLabel();
+					System.out.println();
+					className = className.substring(0, className.length()-5);
+					Tree typeDeclarationTree = classTree.getChildren().get(3).makeLabelsUnique(new HashMap<String, Integer>());
+					Tree sourceMethodTree = typeDeclarationTree.getChildren().get(6)
+							.getChildren().get(6);
+					Tree targetMethodTree = typeDeclarationTree.getChildren().get(7)
+							.getChildren().get(6);
+					double distance = PQGram.getDistance(sourceMethodTree,
+							targetMethodTree, Settings.P, Settings.Q);
+					List<Edit> edits = PQGramRecommendation.getEdits(
+							PQGram.getProfile(sourceMethodTree, Settings.P, Settings.Q),
+							PQGram.getProfile(targetMethodTree,	Settings.P, Settings.Q), 
+							sourceMethodTree, 
+							targetMethodTree);
+					title = String.format("Test results (%s)", className);
+					message = String.format("%d,%d-Gram AST distance: %f\n\nNumber of edits: %d\n\nEdits:\n", Settings.P,Settings.Q, distance, edits.size());
+					for (Edit edit : edits) {
+						message += String.format("%s\n", edit);
+					}
+					message += "\n";
+					
+					List<ExceptionalUnitGraph> methodEUGs = CFGExceptionalUnitGraphAction.getMethodExceptionalUnitGraphs(className, loader);
+					CFG sourceCFG = CFGExceptionalUnitGraphAction.getCFG(methodEUGs.get(1).getBody());
+					CFG targetCFG = CFGExceptionalUnitGraphAction.getCFG(methodEUGs.get(2).getBody());
+					double cfgDistance = PQGram.getDistance(sourceCFG, targetCFG, Settings.P, Settings.Q);
+					message += String.format("%d,%d-Gram CFG distance: %f\n\n", Settings.P, Settings.Q, cfgDistance);
+				} catch (IndexOutOfBoundsException e) {
+					title = "Test error";
+					message = "ASTs not formatted as expected";
+				}
+				this.parentView.showMessage(title, message);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
